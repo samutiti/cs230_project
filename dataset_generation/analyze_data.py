591ffd9491c7e6d513b63bc92435e63d4add60aa
Samantha <samutiti@stanford.edu>
2025-12-01 11:14:46 -0800

    Create analyze_data.py
    to analyze datasets

---
+++ dataset_generation/analyze_data.py
@@ -0,0 +1,37 @@
+# Authors: Samantha Mutiti & Rong Chi
+import os, argparse, json
+import tifffile as tf
+
+def analyze(input_dir, output_file='analysis_results.json'):
+    '''Analyze the dataset in the given directory.'''
+    heights = []
+    widths = []
+    file_list = [f for f in os.listdir(input_dir) if f.endswith('.tif')]
+    for file_name in file_list:
+        file_path = os.path.join(input_dir, file_name)
+        image = tf.imread(file_path)
+        # Assuming the last channel is the mask
+        height, width, _ = image.shape
+        heights.append(height)
+        widths.append(width)
+
+    data_dict = {
+        'average_height': sum(heights) / len(heights),
+        'average_width': sum(widths) / len(widths),
+        'median_height': sorted(heights)[len(heights) // 2],
+        'median_width': sorted(widths)[len(widths) // 2],
+        'num_images': len(file_list),
+        'min_height': min(heights),
+        'max_height': max(heights),  
+    }
+
+    with open(os.path.join(input_dir, output_file), 'w') as f:
+        json.dump(data_dict, f, indent=4)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('input_dir', type=str, required=True, help='Directory to analyze')
+    args = parser.parse_args()
+
+    analyze(args.input_dir)
